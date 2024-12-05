d711a0310b78f9f48cfb94a45663a9e3550ba965
iCrouk <ikercoranti@gmail.com>
2024-12-05 09:51:53 +0100

    [MRG] SYSO.

--- rekuperaketa/src/rekuperaketa/a.java
+++ rekuperaketa/src/rekuperaketa/a.java
@@ -5,6 +5,8 @@ public class a {
 	public static void main(String[] args) {
 		
 		System.out.println("Azterketako Commit-a");
+		
+		System.out.println("Azterketako IKER C");
 		// TODO Auto-generated method stub
 
 	}
